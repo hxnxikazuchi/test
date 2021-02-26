ad7a2f5a69034f52032989d1f3b0d7ea41c2ed90
Ruslan Kupzhassarov <kupzhasarov14@gmail.com>
2021-02-26 18:53:36 +0600

    First task

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,23 @@
 /* jshint browser: true */
 "use strict";
 
-const result = confirm("you ass?");
-console.log(result);
-console.log("PomPom");
-console.log("PomPom1");
+const numberOfFilms = +prompt("How many films have you watched?", "");
+console.log(numberOfFilms);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Last film?", ""),
+      b = prompt("How it was",""),
+      c = prompt("Last film?",""),
+      d = prompt("How it was","");
+
+personalMovieDB.movies[a]=b;
+personalMovieDB.movies[c]=d;
+
+console.log(personalMovieDB);
